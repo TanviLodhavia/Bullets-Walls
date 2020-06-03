a2773036c7dc8434f82c2ce286435b80a09bf4de
TanviLodhavia <65407346+TanviLodhavia@users.noreply.github.com>
2020-06-03 10:44:46 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var bullet;
+var wall;
+var speed,weight;
+var thickness,damage;
+
+function setup() {
+  createCanvas(1600,400);
+  speed=random(223,321);
+  weight=random(30,52);
+  thickness=random(22,83);
+  bullet=createSprite(50,200,30,20);
+  bullet.velocityX= speed;
+  bullet.shapeColor="white";
+  wall=createSprite(1200,200,thickness,height/2);
+  wall.shapeColor=(80,80,80);
+  }
+
+function draw() {
+  background("blue"); 
+  if(wall.x-bullet.x <(bullet.width+wall.width)/2)
+  {damage=0.5*speed*speed*weight/wall.width*wall.width*wall.width;
+  bullet.velocityX=0;
+  if (damage >10){
+    bullet.shapeColor="red";
+  }
+  if (damage <10){
+    bullet.shapeColor="green"
+  }
+}
+  
+  drawSprites();
+}
